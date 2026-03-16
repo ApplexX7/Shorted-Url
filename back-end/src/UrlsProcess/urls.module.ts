61441d70dddd53d88d2428ad3f6044a61309e69f
BADR HILALI <apx.hilali@gmail.com>
2026-03-16 00:11:34 +0000

    Add prisma migration

--- back-end/src/UrlsProcess/urls.module.ts
+++ back-end/src/UrlsProcess/urls.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
 import { UrlsController } from "./urls.controller";
 import { UrlsService } from "./urls.service";
+import { PrismaService } from "src/prismadb/prisma.service";
 
 @Module({
     imports: [],
     controllers: [UrlsController],
-    providers: [UrlsService],
+    providers: [UrlsService, PrismaService],
 })
 export class UrlsModule {}
